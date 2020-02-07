c9132b2d235cc0ab964f79edc45895d1f29753fe
Tore Fadnes <tore.fadnes@ikamr.no>
2020-02-07 15:10:04 +0100

    Fixed folder structure.

--- src/KDRS_Doc_merge/Properties/Resources.Designer.cs
+++ src/KDRS_Doc_merge/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace kdrs_doc_merge.Properties {
+namespace KDRS_Doc_merge.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace kdrs_doc_merge.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("kdrs_doc_merge.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KDRS_Doc_merge.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- src/KDRS_Doc_merge/Properties/Settings.Designer.cs
+++ src/KDRS_Doc_merge/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace kdrs_doc_merge.Properties {
+namespace KDRS_Doc_merge.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
